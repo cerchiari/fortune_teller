e6f4b3d5339daafb8e02ed29355b81be9b55335a
Ana Paula Nunes Cerchiari Almeida <aalmeida2014@kellogg.northwestern.edu>
2013-10-19 18:58:21 -0500

    Define template + random numbers
    Define the template page and also create the 5 random numbers

--- app/controllers/fortune_controller.rb
+++ app/controllers/fortune_controller.rb
@@ -1,5 +1,10 @@
 class FortuneController < ApplicationController
   def rand_numbers
+    @lucky_numbers_1 = rand(1..100)
+    @lucky_numbers_2 = rand(1..100)
+    @lucky_numbers_3 = rand(1..100)
+    @lucky_numbers_4 = rand(1..100)
+    @lucky_numbers_5 = rand(1..100)
     render 'rand_numbers'
   end
 end
